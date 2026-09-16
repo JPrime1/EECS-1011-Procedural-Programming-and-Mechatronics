55cb986882e2502ad8d4644094b48d6ea0a8a670
James <James.Prime.dev@gmail.com>
2026-09-16 10:13:57 -0400

    fixed commenting syntax error

--- Additional_Work/blackboard.m
+++ Additional_Work/blackboard.m
@@ -1,13 +1,14 @@
-//Blackboard problem to introduce MATLAB variables and operations
+% Blackboard problem to introduce MATLAB variables and operations
 
-// Given a parallelogram 
-// a base of 12 units
-// a side of 7 units
-// a height of 5 units
+% Given a parallelogram 
+% a base of 12 units
+% a side of 7 units
+% a height of 5 units
+clc, clear
 base    = 12
 side    = 7
 height  = 5
 
-// Calculate the area and perimeter of the parallelogram
+% Calculate the area and perimeter of the parallelogram
 area        = (base * height)
 perimeter   = (2 * base) + (2 * side)
