1f798ef00a50ffcb0471187305353e57b90864f3
James <James.Prime.dev@gmail.com>
2026-09-16 10:08:41 -0400

    blackboard solution

---
+++ Additional_Work/blackboard.m
@@ -0,0 +1,13 @@
+//Blackboard problem to introduce MATLAB variables and operations
+
+// Given a parallelogram 
+// a base of 12 units
+// a side of 7 units
+// a height of 5 units
+base    = 12
+side    = 7
+height  = 5
+
+// Calculate the area and perimeter of the parallelogram
+area        = (base * height)
+perimeter   = (2 * base) + (2 * side)
